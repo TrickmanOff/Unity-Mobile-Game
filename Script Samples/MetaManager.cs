7d7b9afe64dae85946a6f2c607bd38800e9c04ce
Trickman <egorkadv2002@gmail.com>
2021-02-10 12:55:05 +1000

    script samples

---
+++ Script Samples/MetaManager.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Experimental.TerrainAPI;
+
+public class MetaManager : MonoBehaviour
+{
+    // TO DO move animation controls to another script
+
+    public static MetaManager instance;
+
+    public SetColorInBlock curBlock;
+    public int curBlockIndex;
+    public Transform blocks;
+    [Tooltip("Стоимость покраски одного блока")]
+    public float paintingPrice;
+    [Tooltip("Время траты одной монеты")]
+    public float oneCoinPaintingTime;
+
+    [Tooltip("Кол-во потраченных монет на текущий блок")]
+    public int alreadySpent = 0;
+
+    public Transform forestBlocks;
+    public CubeAnimation animationCube;
+    public float timeBeforeExplosion;
+
+    public CameraSmoothMoving cameraMoving;
+
+    public Animator congratsAnim;
+
+    public bool animating = false;
+
+    public UnityEvent animationStarted;
+    public UnityEvent animationFinished;
+
+    private IEnumerator paintingCoroutine;
+
+    void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+            InitializeMeta();
+            //instance.curBlockIndex = 0;
+            //instance.curBlock = blocks.GetChild(curBlockIndex).GetComponent<SetColorInBlock>();
+        }
+        else
+            Destroy(this.gameObject);
+    }
+
+    public void StartPainting() {
+        paintingCoroutine = Paint();
+        StartCoroutine(paintingCoroutine);
+    }
+
+    public void StopPainting()
+    {
+        StopCoroutine(paintingCoroutine);
+    }
+
+    //paint block with spending only 1 coin (balance has to be greater than 0) smoothly for oneCoinPaintingTime
+    private void SpendOnePoint()
+    {
+        SavingSystem.AddToInt(SavingSystem.coins, -1, 0);
+        SavingSystem.AddToInt(SavingSystem.spentOnCurBlock, 1, 0);
+        alreadySpent++;
+
+        StartCoroutine(curBlock.SmoothAdd(1f / paintingPrice, oneCoinPaintingTime));
+
+        if (alreadySpent == paintingPrice)
+            curBlock.intensity = 1f;
+
+        if (curBlock.intensity == 1f)
+            BlockPainted();
+    }
+
+    private IEnumerator Paint()
+    {
+        if (animating) yield break;
+
+        //TODO animation if balance == 0
+
+        while(!animating && SavingSystem.GetInt(SavingSystem.coins, 0) != 0)
+        {
+            SpendOnePoint();
+            yield return new WaitForSeconds(oneCoinPaintingTime);
+        }
+    }
+
+    private void BlockPainted()
+    {
+        Debug.Log("Block has been painted!!!");
+
+        StartCoroutine(AnimatePaintedBlock());
+
+        alreadySpent = 0;
+        SavingSystem.AddToInt(SavingSystem.curBlockIndex, 1, -1);
+        SavingSystem.SetInt(SavingSystem.spentOnCurBlock, 0);
+    }
+
+    private IEnumerator AnimatePaintedBlock()
+    {
+        animating = true;
+        animationStarted.Invoke();  
+
+        //start congrats animation
+        congratsAnim.gameObject.SetActive(true);
+
+        //look at painted block
+        yield return cameraMoving.LookAtBlock(curBlock.transform, cameraMoving.cameraFlightSpeed1);
+
+        while (congratsAnim.GetCurrentAnimatorStateInfo(0).IsName("CongratsAnimation"))
+            yield return new WaitForEndOfFrame();
+        congratsAnim.gameObject.SetActive(false);
+
+        yield return AnimateNextBlock();
+    }
+
+    private IEnumerator AnimateNextBlock()
+    {
+        animating = true;
+        animationStarted.Invoke();
+        
+        curBlockIndex++;
+
+        //look at the next block
+        yield return cameraMoving.LookAtBlock(blocks.GetChild(curBlockIndex), cameraMoving.cameraFlightSpeed2);
+
+        animationCube.gameObject.SetActive(true);
+        Transform curForestBlock = forestBlocks.GetChild(curBlockIndex);
+
+        //initialize cube's position and size for current forest ground
+        animationCube.InitForGround(curForestBlock.GetChild(0));
+        //start cube's growing
+        yield return animationCube.AnimateGrowing();
+
+        //replace forestBlock with cityBlock
+        curForestBlock.gameObject.SetActive(false);
+        blocks.GetChild(curBlockIndex).gameObject.SetActive(true);
+        curBlock = blocks.GetChild(curBlockIndex).GetComponent<SetColorInBlock>();
+        curBlock.intensity = 0f;
+
+        //waiting
+        yield return new WaitForSeconds(timeBeforeExplosion);
+
+        //explode the cube
+        yield return animationCube.AnimateExplosion();
+
+        animating = false;
+        animationFinished.Invoke();
+    }
+
+    private void InitializeMeta()
+    {
+        Debug.Log("Initializing meta...");
+
+        curBlockIndex = SavingSystem.GetInt(SavingSystem.curBlockIndex, -1);
+
+        //DELETE THIS
+        //SavingSystem.SetInt(SavingSystem.coins, 1000);
+
+
+        //setting cityBlocks active and forestBlocks inactive
+        for (int i = 0; i <= curBlockIndex; i++)
+        {
+            forestBlocks.GetChild(i).gameObject.SetActive(false);
+            blocks.GetChild(i).gameObject.SetActive(true);
+        }
+
+        //start animation
+        if (curBlockIndex == -1)
+        {
+            StartCoroutine(AnimateNextBlock());
+            SavingSystem.AddToInt(SavingSystem.curBlockIndex, 1, -1);
+        }
+        
+        curBlock = blocks.GetChild(curBlockIndex).GetComponent<SetColorInBlock>();
+
+        alreadySpent = SavingSystem.GetInt(SavingSystem.spentOnCurBlock, 0);
+        curBlock.intensity = (float)alreadySpent / paintingPrice;
+
+        //camera position
+        cameraMoving.LookAtBlockInstantly(curBlock.transform);
+    }
+}
---
+++ Script Samples/SetColorInBlock.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetColorInBlock : MonoBehaviour
+{
+    [Range(0f, 1f)] [Tooltip("Показатель бесцветности объекта")]
+    public float intensity = 1f;
+    public List<int> paintingOrder;
+    [Tooltip("Случайный порядок покраски объектов в блоке")]
+    public bool randomOrder = false;
+    [Tooltip("Seed для генерации порядка покраски объектов в блоке")]
+    public int randomSeed = 1;
+
+    private int childrenCount;
+    private float prevIntensity = 1f;
+    private float childIntensityStep;
+    
+    private void Start()
+    {
+        childrenCount = this.transform.childCount;
+        childIntensityStep = 1f / childrenCount;
+
+        for (int i = 0; i < childrenCount; i++)
+            paintingOrder.Add(i);
+        if(randomOrder)
+            SetRandomOrder();
+
+        ApplyIntensity();
+    }
+
+    private void SetRandomOrder()
+    {
+        Random.InitState(randomSeed);
+        for(int i = childrenCount-1; i >= 0; i--)
+        {
+            int t = paintingOrder[i];
+            int randIndex = Random.Range(0, i + 1);
+            paintingOrder[i] = paintingOrder[randIndex];
+            paintingOrder[randIndex] = t;
+        }
+    }
+
+    private void Update()
+    {
+        if (prevIntensity != intensity)
+            ApplyIntensity();
+    }
+
+    // плавно добавляет к intensity add за время time
+    public IEnumerator SmoothAdd(float add, float time)
+    {
+        float timer = 0;
+        while (timer < time)
+        {
+            float curAdd = Mathf.Min(1f - intensity, add * (Time.deltaTime / time));
+            timer += Time.deltaTime;
+
+            intensity += curAdd;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    // присваивает всем детям Intensity = value (для синхронной раскраски)
+    private void SetChildrenIntensityTo(float value)
+    {
+        foreach(Transform child in this.transform)
+        {
+            child.GetComponent<MeshRenderer>().material.SetFloat("_Intensity", value);
+        }
+    }
+    
+    // применяет текущее значение Intensity
+    private void ApplyIntensity()
+    {
+        //Debug.Log("Intensity " + intensity + " was applied. PrevIntensity = " + prevIntensity);
+        int childIndex = Mathf.Min((int) (prevIntensity / childIntensityStep), childrenCount - 1);
+
+        float left = Mathf.Abs(intensity - prevIntensity) * childrenCount;
+
+        while (childIndex >= 0 && childIndex < childrenCount) {
+            float elementIntensity = GetElementIntensity(childIndex);
+            //Debug.Log(childIndex + "th element's intensity = " + elementIntensity);
+
+            if (intensity < prevIntensity)
+            {
+                if(left >= elementIntensity)
+                {
+                    left -= GetElementIntensity(childIndex);
+                    SetElementIntensity(childIndex, 0f);
+
+                    childIndex--;
+                }
+                else
+                {
+                    SetElementIntensity(childIndex, elementIntensity - left);
+                    break;
+                }
+            }
+            else
+            {
+                if(left + elementIntensity >= 1f)
+                {
+                    left -= (1f - elementIntensity);
+                    SetElementIntensity(childIndex, 1f);
+
+                    childIndex++;
+                }
+                else
+                {
+                    SetElementIntensity(childIndex, elementIntensity + left);
+                    break;
+                }
+            }
+            //Debug.Log("Left = " + left);
+        } 
+
+        prevIntensity = intensity;
+    }
+
+    // делает Intensity ребёнка с индексом index равным value
+    private void SetElementIntensity(int index, float value)
+    {
+        index = paintingOrder[index];
+        //Debug.Log("Set " + index + "th element's intensity = " + value + " PrevIntensity = " + GetElementIntensity(index));
+        //Debug.Log(this.transform.GetChild(index).GetComponent<MeshRenderer>().material.name);
+        this.transform.GetChild(index).GetComponent<MeshRenderer>().material.SetFloat("_Intensity", value);
+        //Debug.Log("Current Intensity = " + GetElementIntensity(index));
+    }
+
+    // возвращает Intensity ребёнка с индексом index
+    private float GetElementIntensity(int index)
+    {
+        index = paintingOrder[index];
+        return this.transform.GetChild(index).GetComponent<MeshRenderer>().material.GetFloat("_Intensity");
+    }
+}
